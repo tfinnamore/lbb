724808e4c0635668a51905bcd29d805f255bca49
Tim Finnamore <tfinnamore@gmail.com>
2016-02-17 18:06:03 -0700

    Added some deps and started my controller

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ArticleController extends Controller
+{
+    //
+
+    public function create()
+    {
+        return view('article.create');
+    }
+
+    public function store(Request $request)
+    {
+      dd($request);
+    }
+
+}
--- config/app.php
+++ config/app.php
@@ -155,6 +155,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        'Collective\Html\HtmlServiceProvider',
 
     ],
 
@@ -201,7 +202,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-
+        'Form'      => 'Collective\Html\FormFacade',
+        'Html'      => 'Collective\Html\HtmlFacade',
     ],
 
 ];
